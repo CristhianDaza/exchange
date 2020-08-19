2b4ba2079db4921e3e5dfbebdd8c2ce0f0911a7e
Cristhian Daza <cristian.d.b@hotmail.com>
2020-08-18 19:01:02 -0500

    add coin detail

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
+import CoinDetail from '@/views/CoinDetail.vue'
 import About from '@/views/About.vue'
 import Error from '@/views/Error.vue'
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: CoinDetail
     }
   ]
 })
